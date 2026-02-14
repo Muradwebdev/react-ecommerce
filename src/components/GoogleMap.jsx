7bc1033ffa083edc582d725dd3c34e6e6a6243b3
IsgenderovMurad <isgenderovm985@gmail.com>
2026-02-14 22:26:38 +0400

    about page completed

---
+++ src/components/GoogleMap.jsx
@@ -0,0 +1,11 @@
+const GoogleMap = () => {
+  return (
+    <div className="map=area">
+      <div className="maps">
+        <iframe src="" frameborder="0"></iframe>
+      </div>
+    </div>
+  );
+};
+
+export default GoogleMap;
